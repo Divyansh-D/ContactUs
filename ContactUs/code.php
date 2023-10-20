8f33e0916a51b91635f289edea8582024a897ec7
Divyansh-D <72186135+Divyansh-D@users.noreply.github.com>
2023-10-20 17:39:08 +0530

    Update code.php

--- ContactUs/code.php
+++ ContactUs/code.php
@@ -6,16 +6,20 @@ if (isset($_POST['btns'])) {
     $name = $_POST['uname'];
     $email = $_POST['email'];
     $desc = $_POST['desc'];
-    if(isset($_FILES["image"])){
-        $file_name = $_FILES['image']['name'];
-        $file_size = $_FILES['image']['size'];
-        $file_tmp = $_FILES['image']['tmp_name'];
-        
-        move_uploaded_file($file_tmp, "images/" . $file_name);
-        }
+    if (empty($_FILES['image']['name'])) {
+        echo "Please upload a file.";
+    } if (empty($name) || empty($email) || empty($desc) || !filter_var($email, FILTER_VALIDATE_EMAIL)|| strlen(trim($name))<3) {
+        echo "Validation failed. Please check your input.";
     }
-
+    else{
+    
         try {
+
+            $file_name = $_FILES['image']['name'];
+            $file_size = $_FILES['image']['size'];
+            $file_tmp = $_FILES['image']['tmp_name'];
+        
+            move_uploaded_file($file_tmp, "images/" . $file_name);
 
             $insertquery = "INSERT INTO `contactus` (`email`, `full_name`, `msg`, `dt`)
                 VALUES (:email, :name, :desc, current_timestamp())";
@@ -33,7 +37,7 @@ if (isset($_POST['btns'])) {
             }
             else{
                 $_SESSION['message']="Something went wrong";
-                header("Location: index.php");
+                header("Location: index.html");
                 exit(0);
             }
             
@@ -42,5 +46,7 @@ if (isset($_POST['btns'])) {
         } finally {
             $con = null;
         }
+    }
+}
 
 ?>
