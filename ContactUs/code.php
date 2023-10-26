fc4aab8015bec8b6ee857025ae85de6d23ba4ba1
Divyansh-D <72186135+Divyansh-D@users.noreply.github.com>
2023-10-26 15:24:19 +0530

    Update code.php

--- ContactUs/code.php
+++ ContactUs/code.php
@@ -1,5 +1,13 @@
+
 <?php
 include('dbcon.php');
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require __DIR__ . '/PHPMailer/src/Exception.php';
+require __DIR__ . '/PHPMailer/src/PHPMailer.php';
+require __DIR__ . '/PHPMailer/src/SMTP.php';
 
 session_start();
 if (isset($_POST['btns'])) {
@@ -26,8 +34,11 @@ if (isset($_POST['btns'])) {
         header("Location: index.php");
         exit(0);
     }
+    
     else{
+    
         try {
+
             $file_name = $_FILES['image']['name'];
             $file_size = $_FILES['image']['size'];
             $file_tmp = $_FILES['image']['tmp_name'];
@@ -42,17 +53,43 @@ if (isset($_POST['btns'])) {
             $stmt->bindParam(":name", $name);
             $stmt->bindParam(":desc", $desc);
             $result=$stmt->execute();
+            $mail=new PHPMailer();
+
+                $mail->isSMTP();                                      
+                $mail->Host = ' smtp.gmail.com';  
+                $mail->SMTPAuth = true;                               
+                $mail->Username = 'tilu9454@gmail.com';                 
+                $mail->Password = 'glcgothqdxbvqelp';                    
+                $mail->SMTPSecure = 'ssl';                            
+                $mail->Port = 465; 
+                $mail->isHTML(true);
+                $mail->setFrom('tilu9454@gmail.com', 'Divyansh verma');
+                $mail->addReplyTo('tilu9454@gmail.com', 'Divyansh verma');
+
+                $mail->addAddress($email,$name);
+                $mail->addAddress('tilu9454@gmail.com', 'Divyansh verma');
+                $mail->addAttachment($file_tmp,$file_name);
+                $mail->Subject = "Query form the contactus page";
+                $mail->Body    = $desc;
+                $mail->send();
+
+                if(!$mail->send()) {
+                    echo 'Message could not be sent.';
+                    echo 'Mailer Error: '. $mail->ErrorInfo;
+                }
 
             if($result){
-                $_SESSION['message']="Added successfully";
+                $_SESSION['success message']="Added successfully";
                 header("Location: index.php");
                 exit(0);
             }
             else{
-                $_SESSION['message']="Something went wrong";
-                header("Location: index.html");
+                $_SESSION['error message']="Something went wrong";
+                header("Location: index.php");
                 exit(0);
             }
+            
+            
             
         } catch (PDOException $e) {
             echo "Error!" .$e->getMessage();
